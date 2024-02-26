4fee102cdf0a470c0964adc3b7e546278b59551a
Robert Aradei <aradeirobert@gmail.com>
2024-02-26 20:27:17 +0200

    Added routing for home and settings

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,20 @@
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 
 import Clipboard from './components/clipboard/Clipboard';
+import Settings from './components/settings/Settings';
 
 function App() {
     return (
         <div id="App">
-            <Clipboard/>
+            <HashRouter basename={"/"}>
+                <Routes>
+                    <Route path="/" element={<Clipboard />} />
+                    <Route path="/settings" element={<Settings />} />
+                </Routes>
+            </HashRouter>
         </div>
-    )
+    );
 }
 
 export default App
---
+++ frontend/src/components/settings/Settings.tsx
@@ -0,0 +1,7 @@
+export default function Settings() {
+    return (
+        <div>
+            <a href="#">back</a>
+        </div>
+    )
+}
