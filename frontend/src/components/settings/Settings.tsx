a8860024bf8d90aae04394bea7c0a4e67c04ef73
Robert Aradei <aradeirobert@gmail.com>
2024-02-27 19:55:07 +0200

    Updated settings page

--- frontend/src/components/settings/Settings.tsx
+++ frontend/src/components/settings/Settings.tsx
@@ -1,7 +1,25 @@
+import { useNavigate } from 'react-router-dom';
+import './Settings.css'
+
+import { ArrowLeft } from "react-feather";
+
 export default function Settings() {
+
+    const navigate = useNavigate();
+    const toHome = () => navigate('/#');
+
     return (
-        <div>
-            <a href="#">back</a>
+        <div id="settings">
+            <div id='header'>
+                <h1>Settings</h1>
+                <button className='icon-btn' onClick={toHome}><ArrowLeft /></button>
+            </div>
+
+            <section>
+                <h2>General</h2>
+
+                
+            </section>
         </div>
     )
 }
